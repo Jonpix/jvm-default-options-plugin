3e8f9a7633411f867cace5f5a2d0f99a1b555ae5
John Burnell <46542761+Jonpix@users.noreply.github.com>
2024-04-10 14:15:45 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.16.0"
+    id("org.jetbrains.intellij") version "1.17.3"
     id("com.diffplug.spotless") version "6.15.0"
 }
 
